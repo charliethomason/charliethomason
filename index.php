33b9ef5a6eea668bf8da5694ced694ac36e4a929
Charlie Thomason <thomason.charlie@gmail.com>
2013-10-15 00:44:08 -0500

    Fixed ideas post title CSS styling and global index head width

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
          
 	        <li>
 		        <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-				<div class="catdate"><strong><?php the_time('D, M j, Y'); ?></strong></div>
+				<div class="date"><?php the_time('D, M j, Y'); ?></div>
 			</li>
 			
 			<?php endwhile; ?>
