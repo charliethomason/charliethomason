0624518e23e4a40ec134d7cab427333e9117de5c
Charlie Thomason <thomason.charlie@gmail.com>
2013-11-12 19:53:46 -0600

    Added function to remove inline styles from div.wp-caption

--- functions.php
+++ functions.php
@@ -90,4 +90,33 @@ function more_links_btn($more_link) {
 	return str_replace('class="more-link"','class="more-link btn"',$more_link);
 }
 add_filter('the_content_more_link','more_links_btn');
+
+/**
+ * Remove inline styles from div.wp-caption
+ * Solution found at: wordpress.org/support/topic/stripping-inline-width-style-for-wp_caption-on-images
+ */
+add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
+add_shortcode('caption', 'fixed_img_caption_shortcode');
+function fixed_img_caption_shortcode($attr, $content = null) {
+	if ( ! isset( $attr['caption'] ) ) {
+		if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+			$content = $matches[1];
+			$attr['caption'] = trim( $matches[2] );
+		}
+	}
+	$output = apply_filters('img_caption_shortcode', '', $attr, $content);
+	if ( $output != '' )
+		return $output;
+	extract(shortcode_atts(array(
+		'id'	=> '',
+		'align'	=> 'alignnone',
+		'width'	=> '',
+		'caption' => ''
+	), $attr));
+	if ( 1 > (int) $width || empty($caption) )
+		return $content;
+	if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+	return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" >' 
+			. do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
 ?>
