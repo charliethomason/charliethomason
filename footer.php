fac094cd2b396b8f53b99721f4fb9494168a7961
Charlie Thomason <charlie.thomason@razorfish.com>
2013-12-17 11:55:32 -0600

    Added LS icon & accessibility features to footer

--- footer.php
+++ footer.php
@@ -1,7 +1,9 @@
 </div><!-- div#container -->
 
-<footer>Copyright &copy; <?php echo date('Y'); ?> Bleachwave.com. All Rights Reserved.<br>
-	Website created by <a href="http://bleachwave.com">Charlie Thomason</a>
+<footer role="contentinfo">
+	<p tabindex="0">Copyright &copy; <?php echo date('Y'); ?> Bleachwave.com. All Rights Reserved.<br>
+	Website created by <a href="http://bleachwave.com">Charlie Thomason</a></p>
+	<a id="LS-icon" href="http://laughingsquid.us" rel="nofollow" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/LS-icon01.gif" alt="Powered by Laughing Squid Web Hosting" /></a>
 </footer>
 
 <?php wp_footer(); ?>
