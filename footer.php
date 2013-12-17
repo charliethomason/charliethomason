486804359edad7f18680d77565432da124eaa0aa
Charlie Thomason <thomason.charlie@gmail.com>
2013-12-17 17:56:45 -0600

    Updated Laughing Squid icon in footer

--- footer.php
+++ footer.php
@@ -2,8 +2,8 @@
 
 <footer role="contentinfo">
 	<p tabindex="0">Copyright &copy; <?php echo date('Y'); ?> Bleachwave.com. All Rights Reserved.<br>
-	Website created by <a href="http://bleachwave.com">Charlie Thomason</a></p>
-	<a id="LS-icon" href="http://laughingsquid.us" rel="nofollow" target="_blank"><img src="<?php bloginfo('template_directory'); ?>/images/LS-icon01.gif" alt="Powered by Laughing Squid Web Hosting" /></a>
+	Website created by <a href="http://charliethomason.com">Charlie Thomason</a></p>
+	<a id="LS-icon" href="http://laughingsquid.us" rel="nofollow" target="_blank" title="Powered by Laughing Squid Web Hosting"><img src="<?php bloginfo('template_directory'); ?>/images/LS-icon01.jpg" alt="Powered by Laughing Squid Web Hosting" /></a>
 </footer>
 
 <?php wp_footer(); ?>
