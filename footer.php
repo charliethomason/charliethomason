ac7c5ae931e5b55acc0539959271ec4538822b2a
Charlie Thomason <thomason.charlie@gmail.com>
2014-01-01 11:12:15 -0600

    Updated footer

--- footer.php
+++ footer.php
@@ -1,11 +1,10 @@
 </div><!-- div#container -->
 
 <footer role="contentinfo">
-	<p tabindex="0">Copyright &copy; <?php echo date('Y'); ?> Bleachwave.com. All Rights Reserved.<br>
+	<p tabindex="0">Copyright &copy; <?php echo date('Y'); ?> Charlie Thomason. All Rights Reserved.<br>
 	Website designed <span class="fancy-amp">&amp;</span> built by <a href="http://charliethomason.com">Charlie Thomason</a><br>
 	Powered by:</p>
 	<ul>
-		<li><a id="html5-icon" href="http://www.w3.org/TR/html5/" rel="nofollow" target="_blank" title="HTML5"><img src="<?php bloginfo('template_directory'); ?>/images/skills/html5-02.png" alt="HTML5 logo" /></a></li>
 		<li><a id="wp-icon" href="http://wordpress.org" rel="nofollow" target="_blank" title="WordPress"><img src="<?php bloginfo('template_directory'); ?>/images/skills/wordpress-01.png" alt="WordPress logo" /></a></li>
 		<li><a id="LS-icon" href="http://laughingsquid.us" rel="nofollow" target="_blank" title="Laughing Squid Web Hosting"><img src="<?php bloginfo('template_directory'); ?>/images/LS-icon02.jpg" alt="Laughing Squid Web Hosting logo" /></a></li>
 	</ul>
